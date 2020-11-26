02278ed2e88efa481756257bcbd11f7a7ca76152
emmanuelZ7 <43683975+emmanuelZ7@users.noreply.github.com>
2020-11-26 21:38:18 +0100

    Add files via upload

---
+++ project.js
@@ -0,0 +1,10 @@
+function toggle(){
+    var content=document.getElementById("more-popup");
+    content.classList.toggle("active");
+
+    
+    var blur= document.getElementById("blur");
+	blur.classList.toggle("active");
+
+
+}
